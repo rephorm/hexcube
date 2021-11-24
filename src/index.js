5d9ec2777cac992e20ca15b8bc1ae66f4d19c628
Brian Mattern <rephorm@rephorm.com>
2021-11-23 22:04:56 -0800

    Switch to accurate hexes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
-import hexTilesImg from './assets/hex_tiles.png'
+import hexTileImg from './assets/hex-tile.png'
 import cubeImg from './assets/cube-angle.png'
+import {Hex, HexGrid} from './hex.js'
 
 class Main extends Phaser.Scene
 {
@@ -26,9 +27,9 @@ class Main extends Phaser.Scene
 
     preload ()
     {
-        this.load.spritesheet('hextiles', hexTilesImg, {
-            frameWidth: 32,
-            frameHeight: 32,
+        this.load.spritesheet('hextile', hexTileImg, {
+            frameWidth: 33,
+            frameHeight: 33,
         })
         this.load.spritesheet('cube', cubeImg, {
             frameWidth: 32,
@@ -38,20 +39,20 @@ class Main extends Phaser.Scene
       
     create ()
     {
-        let frames = this.anims.generateFrameNumbers('hextiles', {start: 0, end: 13})
-        this.anims.generateFrameNumbers('hextiles', {start: 12, end: 1, outputArray: frames})
+        var frames = this.anims.generateFrameNumbers('hextile', {start: 0, end: 17})
+        //this.anims.generateFrameNumbers('hextile', {start: 16, end: 1, outputArray: frames});
         this.anims.create({
             key: 'raise',
             frames: frames,
             frameRate: 16,
-            repeat: -1
+            repeat: -1,
+            yoyo: 1,
         });
         this.anims.create({
             key: 'up-right',
             frames: this.anims.generateFrameNumbers('cube', {start: 0, end: 5}),
             frameRate: 8,
             repeat: -1,
-            bounce: 1,
 
         })
         this.anims.create({
@@ -59,35 +60,30 @@ class Main extends Phaser.Scene
             frames: this.anims.generateFrameNumbers('cube', {start: 6, end: 11}),
             frameRate: 8,
             repeat: -1,
-            bounce: 1,
         })
         this.anims.create({
             key: 'down-right',
             frames: this.anims.generateFrameNumbers('cube', {start: 12, end: 17}),
             frameRate: 8,
             repeat: -1,
-            bounce: 1,
         })
         this.anims.create({
             key: 'down-left',
             frames: this.anims.generateFrameNumbers('cube', {start: 18, end: 23}),
             frameRate: 8,
             repeat: -1,
-            bounce: 1,
         })
         this.anims.create({
             key: 'up',
             frames: this.anims.generateFrameNumbers('cube', {start: 24, end: 29}),
             frameRate: 8,
             repeat: -1,
-            bounce: 1,
         })
         this.anims.create({
             key: 'down',
             frames: this.anims.generateFrameNumbers('cube', {start: 30, end: 35}),
             frameRate: 8,
             repeat: -1,
-            bounce: 1,
         })
         var cx = this.scale.width / 2
         var cy = this.scale.height / 2
@@ -99,13 +95,13 @@ class Main extends Phaser.Scene
                 var d = Math.max(Math.abs(q), Math.abs(r), Math.abs(s))
                 if (d > 10) continue;
                 var x = cx + 24 * q ;
-                var y = cy + 15 * r + 8*q;
+                var y = cy + 16 * r + 8*q;
                 var frame = Phaser.Math.Between(0, 14)
                 frame = 0
-                var o = this.add.sprite(x, y, 'hextiles', Math.floor(frame))
+                var o = this.add.sprite(x, y, 'hextile', Math.floor(frame))
                 this.tiles.set(`${q},${r}`, o)
                 o.setDepth(r*10)
-                o.play({key: 'raise', delay: 1000/8.0 * (d+0.5*Math.abs(q))})
+                //o.play({key: 'raise', delay: 1000/8.0 * (d+0.5*Math.abs(q))})
             }
         }
 
@@ -122,11 +118,11 @@ class Main extends Phaser.Scene
     setupInput() {
         var keys = [
             {key: 'D', anim: 'down-right', move: {dx: 24, dy: 8}},
-            {key: 'S', anim: 'down', move: {dx: 0, dy: 15}},
-            {key: 'A', anim: 'down-left', move: {dx: -24, dy: 7}},
+            {key: 'S', anim: 'down', move: {dx: 0, dy: 16}},
+            {key: 'A', anim: 'down-left', move: {dx: -24, dy: 8}},
             {key: 'Q', anim: 'up-left', move: {dx: -24, dy: -8}},
-            {key: 'W', anim: 'up', move: {dx: 0, dy: -15}},
-            {key: 'E', anim: 'up-right', move: {dx: 24, dy: -7}},
+            {key: 'W', anim: 'up', move: {dx: 0, dy: -16}},
+            {key: 'E', anim: 'up-right', move: {dx: 24, dy: -8}},
         ];
         for (let info of keys) {
             this.input.keyboard.on('keydown-'+info.key, ()=>{
@@ -203,7 +199,7 @@ class Main extends Phaser.Scene
         var dx = x - this.cx
         var dy = y - this.cy
         var q = Math.round(dx / 24)
-        var r = Math.round((dy - 8 * q) / 15)
+        var r = Math.round((dy - 8 * q) / 16)
         return [q,r]
     }
 
@@ -229,4 +225,4 @@ const config = {
     backgroundColor: 0xffffee,
 };
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(config)
